1e61049f85bc7d715c698c3b1a280c9acea4b7ad
Diego Andrés Alvarez Marín <daalvarez@unal.edu.co>
2025-06-20 18:53:22 -0500

    Mejorando el código

--- codigo/09_torsion/torsion_cuadrado/calc_psi_phi_cuadrado.m
+++ codigo/09_torsion/torsion_cuadrado/calc_psi_phi_cuadrado.m
@@ -1,83 +1,99 @@
-function [psi,phi,x,y] = calc_psi_phi_cuadrado(a,b,G,theta)
-% Calculo de la funcion de alabeo psi y la funcion de tension de Prandtl
+function [psi, phi, x, y] = calc_psi_phi_cuadrado(a, b, G, theta)
+% Cálculo de la función de alabeo psi y la función de tensión de Prandtl
 % phi para un rectangulo de lados 2a y 2b.
+%
 % Recuerde que:
 % w(x,y) = theta*psi(x,y)
 
 %{
 a = 0.01;
 b = 0.01;
-E  = 21e6;         % kPa = 21GPa - modulo de Young del acero
+E  = 21e6;         % kPa = 21GPa - módulo de Young del acero
 nu = 0.28;         % coeficiente de Poisson del acero
-G  = E/(2*(1+nu)); % modulo de cortante
+G  = E/(2*(1+nu)); % módulo de cortante
 theta = 0.05;      % rad/m
+[psi, phi, x, y] = calc_psi_phi_cuadrado(a, b, G, theta);
 %}
 
-xx = linspace(-a,a,21); nx = length(xx);
-yy = linspace(-b,b,21); ny = length(yy);
+%% Se inicializan algunas variables
+nx = 21; xx = linspace(-a, a, nx);
+ny = 21; yy = linspace(-b, b, ny);
 
 [x,y] = meshgrid(xx,yy);
 
+%% Cálculo de la función de alabeo de Saint-Venant
 n = 0:250; %0,1,2,3... to Infinity
 kn = (2*n + 1)*pi/(2*a);
 tmp1 = ((32*a^2)/(pi^3))*((-1).^n)./(((2*n+1).^3) .* cosh(kn.*b));
 psi1 = zeros(ny,nx);
 for i = 1:ny
    for j = 1:nx
-      tmp2 = sinh(kn.*y(i,j));
-      tmp = tmp1 .* tmp2;
+      tmp2 = sin(kn.*x(i,j)) .* sinh(kn.*y(i,j));
+
+      % se da manejo a ciertas indeterminaciones matemáticas
+      tmp  = tmp1 .* tmp2;
       tmp((tmp1==0) & isinf(tmp2)) = 0;
-      psi1(i,j) = sum(tmp .* sin(kn.*x(i,j)));
+      
+      psi1(i,j) = sum(tmp);
    end
 end
 psi = x.*y - psi1;
 
+%% Cálculo de la función de tensión de Prandtl
 n = 1:2:500; %1,3,5,... to Infinity
-C2 = ((32*G*theta*a^2)/(pi^3)) .*((-1).^((n-1)/2))./(n.^3);
+tmp1 = ((32*G*theta*a^2)/(pi^3)) .*((-1).^((n-1)/2))./(n.^3);
 
 phi = zeros(ny,nx);
 for i = 1:ny
    for j = 1:nx
-      tmp1 = cosh(n*pi*y(i,j)/(2*a));
-      tmp2 = cosh(n*pi*b/(2*a));
-      tmp = tmp1./tmp2;
-      tmp(isinf(tmp1) & isinf(tmp2)) = 1;
-      phi(i,j) = sum(C2 .* (1 - tmp) .* cos(n*pi*x(i,j)/(2*a)));
+      tmp2 = cosh(n*pi*y(i,j)/(2*a));
+      tmp3 = cosh(n*pi*b/(2*a));
+
+      % se da manejo a ciertas indeterminaciones matemáticas
+      tmp4 = tmp2./tmp3;
+      tmp4(isinf(tmp2) & isinf(tmp3)) = 1;
+
+      phi(i,j) = sum(tmp1 .* (1 - tmp4) .* cos(n*pi*x(i,j)/(2*a)));
    end
 end
 
+% La función de tensión de Prandtl es nula en el contorno
 phi([1 ny],:) = 0;
 phi(:,[1 nx]) = 0;
 
-maxphi = max(max(phi));
 %% Grafico que representa la funcion de tension de Prandtl
 figure
-surf(x,y,phi);    % Hacer un dibujo en 2D de phi
+mesh(x,y,phi);    % Hacer un dibujo en 2D de phi
 hold on;          % Permitir hacer modificaciones al dibujo anterior
-shading interp;   % Hacer interpolacion de colores
+%shading interp;  % Hacer interpolación de colores
 colorbar          % Poner la barra de colores
-contour(xx,yy,phi,20,'k'); % Dibujar 20 curvas de nivel (isobaras)
-title({'Función de tension de Prandtl \phi','(solucion teorica)'},'FontSize', 18) % Escribir el titulo
-%axis([-1 1 -1 1 0 maxphi]);  
+contour(xx,yy,phi,20,'k'); % Dibujar 20 curvas de nivel
+title({'Función de tensión de Prandtl \phi(x,y)', ...
+       '(solución teórica)'},'FontSize', 18);
 axis tight
 
 % Dibujo los bordes de la figura
-%plot([-1 1 1 -1 -1], [-1 -1 1 1 -1], 'k-', 'LineWidth',5)
+plot([-a a a -a -a], [-b -b b b -b], 'k-', 'LineWidth',5)
 
-
-%% Calculo el alabeo
-w = theta*psi;
 
 %% Grafico el alabeo w = theta*psi
+w = theta*psi;
+
 figure
-mesh(x,y,w);    % Hacer un dibujo en 2D del alabeo
+mesh(x,y,w);      % Hacer un dibujo en 2D del alabeo
 hold on;          % Permitir hacer modificaciones al dibujo anterior
-shading interp;   % Hacer interpolacion de colores
+%shading interp;  % Hacer interpolación de colores
 colorbar          % Poner la barra de colores
 axis tight
-contour(xx,yy,w,20,'k'); % Dibujar 20 curvas de nivel (isobaras)
-title({'Desplazamiento en z (alabeo) w(x,y) = theta*\Psi(x,y)','(solucion teorica)'},'FontSize', 18) % Escribir el titulo
-%plot([-1 1 1 -1 -1], [-1 -1 1 1 -1], 'k-', 'LineWidth',5)
+contour(xx,yy,w,20,'k'); % Dibujar 20 curvas de nivel
+title({'Desplazamiento en z (alabeo) w(x,y) = \theta\Psi(x,y)', ...
+       '(solución teórica)'},'FontSize', 18);
 xlabel('Eje x');
 ylabel('Eje y');
 zlabel('Alabeo w(x,y)');
+
+% Dibujo los bordes de la figura
+plot([-a a a -a -a], [-b -b b b -b], 'k-', 'LineWidth',5)
+
+%% bye, bye!!
+return
--- codigo/09_torsion/torsion_cuadrado/show_torsion_cuadrado.m
+++ codigo/09_torsion/torsion_cuadrado/show_torsion_cuadrado.m
@@ -2,234 +2,138 @@ clear
 clc
 close all
 
-%% Torsion de una barra rectangular
-E  = 21e6;         % kPa = 21GPa - modulo de Young del acero
+%% Torsión de una barra con sección transversal cuadrada
+E  = 21e6;         % [kPa] = 21GPa - módulo de Young del acero
 nu = 0.28;         % coeficiente de Poisson del acero
-G  = E/(2*(1+nu)); % modulo de cortante
-theta = 0.05;      % rad/m
-delta = 0.001;     % espaciado de la malla
+G  = E/(2*(1+nu)); % [kPa] módulo de cortante 
+L  = 1;            % [m]
+rho_L = 0.05;      % [rad]
+theta = rho_L/L;   % [rad/m]
+delta = 0.001;     % [m] espaciado de la malla
 
-%% Aqui hice copy/paste de MS EXCEL de la funcion de tension de Prandtl phi
-phi = xlsread('torsion_cuadrado.xlsx','Prandtl','phi'); 
+%% Coordenadas del borde de la barra cuadrada
+global a b borde
+a = 0.01; % [m]
+b = 0.01; % [m]
+
+% coordenadas del borde (x,y) que forman el perímetro
+borde = ...
+    [-a -b 
+      a -b
+      a  b
+     -a  b
+     -a -b ]; % observe que se repite la primera fila
+
+%% Leer de MS EXCEL de la función de tensión de Prandtl phi
+phi = readmatrix('torsion_cuadrado.xlsx', 'Sheet', 'Prandtl', 'Range','phi');
 phi = flipud(phi);
 
-%% Aqui hice copy/paste de MS EXCEL de la funcion de alabeo psi
-psi = xlsread('torsion_cuadrado.xlsx','Alabeo','psi');
+%% Leer de MS EXCEL de la función de alabeo psi
+psi = readmatrix('torsion_cuadrado.xlsx', 'Sheet', 'Alabeo', 'Range','psi');
 psi = flipud(psi);
 
-%% Aqui defino las coordenas en x e y que estoy manejando
+%% Definir las coordenadas en x e y
 x = -0.01:delta:0.01;
 y = -0.01:delta:0.01;
 
-%% Al ejecutar "help meshgrid obtengo":
-%    [X,Y] = MESHGRID(x,y) transforms the domain specified by vectors
-%    x and y into arrays X and Y that can be used for the evaluation
-%    of functions of two variables and 3-D surface plots.
-%    The rows of the output array X are copies of the vector x and
-%    the columns of the output array Y are copies of the vector y.
 [xx,yy] = meshgrid(x,y);
 
-%% Creo funciones para calcular las derivadas usando diferencias centrales
-deriv_x = @(f) (f(2:end-1,3:end)-f(2:end-1,1:end-2))/(2*delta);
-deriv_y = @(f) (f(3:end,2:end-1)-f(1:end-2,2:end-1))/(2*delta);
+%% Funciones para calcular las derivadas usando diferencias centrales
+d_dx = @(f) (f(2:end-1,3:end) - f(2:end-1,1:end-2))/(2*delta);
+d_dy = @(f) (f(3:end,2:end-1) - f(1:end-2,2:end-1))/(2*delta);
 
-%% Calculo los esfuerzos
-txz = G*theta*(deriv_x(psi) - yy);
-tyz = G*theta*(deriv_y(psi) + xx);
+%% Cálculo de los esfuerzos
+txz = G*theta*(d_dx(psi) - yy);
+tyz = G*theta*(d_dy(psi) + xx);
 
-%% Grafico que representa los esfuerzos tau_xz
-figure             % Preparar al MATLAB para hacer una figura
-pcolor(xx,yy,txz); % Hacer un dibujo en 2D de los esfuerzos en tau_xz
-hold on;           % Permitir hacer modificaciones al dibujo anterior
-shading interp;    % Hacer interpolacion de colores
-hcb = colorbar;    % Poner la barra de colores
-axis equal         % Hacer un grafico con coordenadas de igual tamaño
-[C,h] = contour(xx,yy,txz,20,'k'); % Dibujar 20 curvas de nivel (isobaras)
-set(h,'ShowText','on','TextStep',get(h,'LevelStep')*2);
-title('Esfuerzos cortantes \tau_{xz}(x,y) sobre la cara \Omega_L [kPa]');
-%title('Esfuerzos cortantes \tau_{xz}','FontSize', 18) % Escribir el titulo
-% Defino las coordenadas que quiero visualizar [xmin xmax ymin ymax]
-axis tight
-%axis([-0.01 0.01 -0.01 0.01]);
-% Dibujo los bordes de la figura
-%plot([-0.01 0.01 0.01 -0.01 -0.01], [-0.01 -0.01 0.01 0.01 -0.01], 'k-', 'LineWidth',5)
-xlabel('x [cm]'); ylabel('y [cm]');
+%% Eliminar los nodos ficticios alrededor de psi
+psi = psi(2:end-1,2:end-1);
 
-% Cree un archivo con el grafico listo para incluir en LaTeX
-%set(gca,'FontSize',16);   % Tamano de la fuente en los numeros de los ejes x e y
-%set(hcb,'FontSize',16);   % Tamano de la fuente en la barra de colores
-colormap jet
-%print('-depsc','txz_square.eps'); 
-print('txz_square.png','-dpng','-r200');
+%% Figuras de los esfuerzos cortantes
+figure
+plot_esf_2d(xx,yy,txz, ...
+    'Esfuerzos cortantes \tau_{xz}(x,y) sobre la cara \Omega_L [kPa]', ...
+    'txz_square.eps');
 
+figure
+plot_esf_2d(xx,yy,tyz, ...
+    'Esfuerzos cortantes \tau_{yz}(x,y) sobre la cara \Omega_L [kPa]', ...
+    'tyz_square.eps');
 
-%% Grafico que representa los esfuerzos tau_yz
-figure             % Preparar al MATLAB para hacer una figura
-pcolor(xx,yy,tyz); % Hacer un dibujo en 2D de los esfuerzos en tau_yz
-hold on;           % Permitir hacer modificaciones al dibujo anterior
-shading interp;    % Hacer interpolacion de colores
-hcb = colorbar;    % Poner la barra de colores
-axis equal         % Hacer un grafico con coordenadas de igual tamaño
-[C,h] = contour(xx,yy,tyz,20,'k'); % Dibujar 20 curvas de nivel (isobaras)
-set(h,'ShowText','on','TextStep',get(h,'LevelStep')*2);
-title('Esfuerzos cortantes \tau_{yz}(x,y) sobre la cara \Omega_L [kPa]');
-%title('Esfuerzos cortantes \tau_{yz}','FontSize', 18) % Escribir el titulo
-% Defino las coordenadas que quiero visualizar [xmin xmax ymin ymax]
-axis tight
-% Dibujo los bordes de la figura
-%plot([-0.01 0.01 0.01 -0.01 -0.01], [-0.01 -0.01 0.01 0.01 -0.01], 'k-', 'LineWidth',5);
-xlabel('x [cm]'); ylabel('y [cm]');
+%% Figura de los esfuerzos tau
+tau = hypot(txz, tyz);
 
-% Cree un archivo con el grafico listo para incluir en LaTeX
-%set(gca,'FontSize',16);   % Tamano de la fuente en los numeros de los ejes x e y
-%set(hcb,'FontSize',16);   % Tamano de la fuente en la barra de colores
-colormap jet
-%print('-depsc','tyz_square.eps'); 
-print('tyz_square.png','-dpng','-r200');
+figure
+plot_esf_3d(xx, yy, tau, ...
+    'Esfuerzos cortantes \tau(x,y) sobre la cara \Omega_L [kPa]')
+hold on
 
-
-tau = sqrt(txz.^2 + tyz.^2);
-maxtau = max(max(tau));
-%% Grafico que representa los esfuerzos tau
-figure            % Preparar al MATLAB para hacer una figura
-h = surf(xx,yy,tau);  % Hacer un dibujo en 2D de los esfuerzos en tau_yz
-hold on;          % Permitir hacer modificaciones al dibujo anterior
-shading interp;   % Hacer interpolacion de colores
-set(h','EdgeColor','k','EdgeAlpha',0.5)
-hcb = colorbar;   % Poner la barra de colores
-contour(xx,yy,tau,20,'k'); % Dibujar 20 curvas de nivel (isobaras)
-title('Esfuerzos cortantes \tau_{xz}(x,y) sobre la cara \Omega_L [kPa]');
-%title('Esfuerzos cortantes \tau','FontSize', 18) % Escribir el titulo
-% Defino las coordenadas que quiero visualizar [xmin xmax ymin ymax zmin zmax]
-axis tight;
-% Dibujo los bordes de la figura
-%plot([-0.01 0.01 0.01 -0.01 -0.01], [-0.01 -0.01 0.01 0.01 -0.01], 'k-', 'LineWidth',5)
 % Dibujo los esfuerzos cortantes
-quiver(xx,yy,txz,tyz);
-xlabel('x [cm]'); ylabel('y [cm]');
+quiver(xx,yy, txz,tyz, 'LineWidth', 1.5, 'Color', 'k');
 
-% Cree un archivo con el grafico listo para incluir en LaTeX
-%set(gca,'FontSize',16);   % Tamano de la fuente en los numeros de los ejes x e y
-%set(hcb,'FontSize',16);   % Tamano de la fuente en la barra de colores
-colormap jet
-%print('-depsc','tau_square_3d.eps'); 
-print('tau_square_3d.png','-dpng','-r200');
+zlabel('Esfuerzo cortante \tau(x,y) [kPa]');
+exportgraphics(gcf,'tau_square_3d.eps', ...
+    'BackgroundColor','none',...
+    'ContentType','vector')
 
-maxphi = max(max(phi));
-%% Grafico que representa la funcion de tension de Prandtl
-figure            % Preparar al MATLAB para hacer una figura
-h = surf(xx,yy,phi);  % Hacer un dibujo en 2D de phi
-hold on;          % Permitir hacer modificaciones al dibujo anterior
-shading interp;   % Hacer interpolacion de colores
-set(h','EdgeColor','k','EdgeAlpha',0.5)
-hcb = colorbar;   % Poner la barra de colores
-%contour(xx,yy,phi,20,'k'); % Dibujar 20 curvas de nivel (isobaras)
-title('Función de tensión de Prandtl \phi') % Escribir el titulo
-% Defino las coordenadas que quiero visualizar [xmin xmax ymin ymax zmin zmax]
-axis tight
-%axis([-0.01 0.01 -0.01 0.01 0 maxphi]);
-% Dibujo los bordes de la figura
-%plot([-0.01 0.01 0.01 -0.01 -0.01], [-0.01 -0.01 0.01 0.01 -0.01], 'k-', 'LineWidth',5)
-xlabel('x [cm]'); ylabel('y [cm]');
-%zlabel('phi','FontSize',16);
+%% Función de tension de Prandtl 3D
+figure
+plot_esf_3d(xx, yy, phi, ...
+    {'Función de tension de Prandtl \phi(x,y)','(solución numérica)'})
+hold on
 
-% Cree un archivo con el grafico listo para incluir en LaTeX
-%set(gca,'FontSize',16);   % Tamano de la fuente en los numeros de los ejes x e y
-%set(hcb,'FontSize',16);   % Tamano de la fuente en la barra de colores
-colormap jet
-%print('-depsc','prandtl_square_3d.eps'); 
-print('prandtl_square_3d.png','-dpng','-r200');
-
-
-%% Grafico que representa la funcion de tension de Prandtl
-figure            % Preparar al MATLAB para hacer una figura
-pcolor(xx,yy,phi);% Hacer un dibujo en 2D de los esfuerzos en tau_yz
-hold on;          % Permitir hacer modificaciones al dibujo anterior
-contour(xx,yy,phi,10,'k'); % Dibujar 20 curvas de nivel (isobaras)
-shading interp;   % Hacer interpolacion de colores
-colormap pink     % Que colores quiero utilizar?
-hcb = colorbar;   % Poner la barra de colores
-axis equal        % Hacer un grafico con coordenadas de igual tamaño
-title({'Curvas de nivel de la función de tensión de Prandtl \phi',...
-   'Se muestran ademas los esfuerzos tangenciales',}) % Escribir el titulo
-% Defino las coordenadas que quiero visualizar [xmin xmax ymin ymax]
-axis([-0.011 0.011 -0.011 0.011]);
-% Dibujo los bordes de la figura
-%plot([-0.01 0.01 0.01 -0.01 -0.01], [-0.01 -0.01 0.01 0.01 -0.01], 'k-', 'LineWidth',5)
 % Dibujo los esfuerzos cortantes
-quiver(xx,yy,txz,tyz,'Color','m','LineWidth', 1.5);
-%xlabel('Eje x','FontSize',16);
-%ylabel('Eje y','FontSize',16);
+quiver(xx,yy, txz,tyz, 'LineWidth', 1.5, 'Color', 'k');
 
-%% Encontrar el centro de torsion
+zlabel('Función de tensión de Prandtl, \phi(x, y) [kPa*m]');
+exportgraphics(gcf,'prandtl_square_3d.eps', ...
+    'BackgroundColor','none',...
+    'ContentType','vector')
+
+%% Función de tension de Prandtl 2D
+figure
+plot_esf_2d(xx,yy,phi, ...
+    {'Curvas de nivel de la función de tensión de Prandtl \phi(x,y)',...
+     'Se muestran ademas los esfuerzos cortantes'});
+hold on;
+
+% Dibujo los esfuerzos cortantes
+quiver(xx,yy, txz,tyz, 'LineWidth', 1.5, 'Color', 'k');
+
+%% Encontrar el centro de torsión
 options = optimset('fminsearch');
 options.MaxFunEvals = 6000;
 ct = fminsearch(@(x) -interp2(xx,yy,phi,x(:,1),x(:,2)), 0.001*[rand rand]-0.0005);
-h = plot(ct(1), ct(2), 'k*');
-legend(h, 'Centro de torsión','Location','NorthEast');
-
-% Cree un archivo con el grafico listo para incluir en LaTeX
-%title(' ');               % Escribir el titulo
-%set(gca,'FontSize',16);   % Tamano de la fuente en los numeros de los ejes x e y
-%set(hcb,'FontSize',16);   % Tamano de la fuente en la barra de colores
-colormap jet
-xlabel('x [cm]'); ylabel('y [cm]');
-%print('-depsc','prandtl_square_2d.eps'); 
-print('prandtl_square_2d.png','-dpng','-r200');
-
+h = plot(ct(1), ct(2), 'r*', 'MarkerSize',15);
+legend(h, 'Centro de torsion','Location','Best');
 
 %% Calcular el momento torsor equivalente
-%fprintf('Calculanto momento torsor... un momento por favor.\n')
 % q = dblquad(fun,xmin,xmax,ymin,ymax)
-Mt = 2*dblquad(@(x,y) interp2(xx,yy,phi, x,y),-0.01,0.01,-0.01,0.01);
+Mt = 2*dblquad(@(x,y) interp2(xx,yy,phi, x,y), -a, a, -b, b);
 texto_Mt = sprintf('Momento torsor = %g kN-m',Mt);
-%xlabel(texto_Mt,'FontSize', 16);
+xlabel(texto_Mt,'FontSize', 16);
 fprintf('%s\n',texto_Mt);
 
+exportgraphics(gcf,'prandtl_square_2d.eps', ...
+    'BackgroundColor','none', ...
+    'ContentType','vector')
+
 %% Calcular la inercia torsional J
-% fprintf('Calculanto la inercia torsional J ... un momento por favor.\n')
 % q = dblquad(fun,xmin,xmax,ymin,ymax)
 % Mt2 = dblquad(@(x,y) interp2(xx,yy,xx.*tyz - yy.*txz, x,y),-0.01,0.01,-0.01,0.01);
 J = Mt/(G*theta);
 fprintf('J = %g m^4\n',J);
 
-%% Elimino los nodos ficticios de psi
-psi = psi(2:end-1,2:end-1);
-
-%% Calculo el alabeo
+%% Graficar el alabeo w = theta*psi
 w = theta*psi;
 
-%% Grafico el alabeo w = theta*psi
-figure            % Preparar al MATLAB para hacer una figura
-h= surf(xx,yy,w);    % Hacer un dibujo en 2D del alabeo
-hold on;          % Permitir hacer modificaciones al dibujo anterior
-%alpha 0.9
-shading interp;   % Hacer interpolacion de colores
-set(h','EdgeColor','k','EdgeAlpha',0.5)
-colorbar          % Poner la barra de colores
-axis tight
-contour(xx,yy,w,20,'k'); % Dibujar 20 curvas de nivel
-title('Desplazamiento en z (alabeo) w(x,y) =  \theta\Psi(x,y)') % Escribir el titulo
-% Dibujo los bordes de la figura
-%plot([-0.01 0.01 0.01 -0.01 -0.01], [-0.01 -0.01 0.01 0.01 -0.01], 'k-', 'LineWidth',5)
-xlabel('x [cm]'); ylabel('y [cm]');
-zlabel('Alabeo w(x,y) [cm]');
+figure
+plot_esf_3d(xx, yy, w, ...
+    {'Desplazamiento en z (alabeo) w(x,y) = \theta*\Psi(x,y)', ...
+     '(solución numérica)'}, ...
+    'alabeo_square.eps')
 
-
-% Cree un archivo con el grafico listo para incluir en LaTeX
-%title(' ');               % Escribir el titulo
-%set(gca,'FontSize',16);   % Tamano de la fuente en los n�meros de los ejes x e y
-%set(hcb,'FontSize',16);   % Tamano de la fuente en la barra de colores
-colormap jet
-%print('-depsc','alabeo_square.eps'); 
-print('alabeo_square.png','-dpng','-r200');
-
-%return;
-
-%% Calculo la respuesta teorica y calculo la diferencia con la estimacion
-% numerica
-a = 0.01;
-b = 0.01;
+%% Comparación de solución analítica con cálculo numérico
 [psi2,phi2,x2,y2] = calc_psi_phi_cuadrado(a,b,G,theta);
 
 diffphi = abs(phi-phi2)./phi2;  
@@ -241,38 +145,125 @@ diffpsi = abs(psi-psi2)./psi2;
 diffpsi(isnan(diffpsi)) = 0; % Alabeo
 diffpsi(isinf(diffpsi)) = 0; % Alabeo
 
-%% Grafico que representa la diferencia en error de la funcion de tension de Prandtl
-figure            % Preparar al MATLAB para hacer una figura
-h = surf(xx,yy,diffphi); % Hacer un dibujo en 2D de los esfuerzos en tau_yz
-hold on;          % Permitir hacer modificaciones al dibujo anterior
-contour(xx,yy,diffphi,20,'k'); % Dibujar 20 curvas de nivel (isobaras)
-shading interp;   % Hacer interpolacion de colores
-set(h','EdgeColor','k','EdgeAlpha',0.5)
-colorbar          % Poner la barra de colores
-%axis equal        % Hacer un grafico con coordenadas de igual tamaño
-title({'Error en el cálculo de la función de Prandtl \phi',...
-   'Teórico vs diferencias finitas',},'FontSize', 18) % Escribir el titulo
-% Defino las coordenadas que quiero visualizar [xmin xmax ymin ymax]
-axis([-0.011 0.011 -0.011 0.011]);
-% Dibujo los bordes de la figura
-%plot([-0.01 0.01 0.01 -0.01 -0.01], [-0.01 -0.01 0.01 0.01 -0.01], 'k-', 'LineWidth',5)
+%% Error relativo en el cálculo de la función de tensión de Prandtl
+figure
+plot_esf_3d(xx, yy, diffphi, ...
+    {'Error relativo en el cálculo de la función de Prandtl \phi(x,y)', ...
+     'Teórico vs diferencias finitas'})
 
-%% Grafico que representa la diferencia en error de la funcion de alabeo
-figure            % Preparar al MATLAB para hacer una figura
-surf(xx,yy,diffpsi); % Hacer un dibujo en 2D de los esfuerzos en tau_yz
-hold on;          % Permitir hacer modificaciones al dibujo anterior
-contour(xx,yy,diffpsi, 20,'k'); % Dibujar 20 curvas de nivel (isobaras)
-shading interp;   % Hacer interpolacion de colores
-colorbar          % Poner la barra de colores
-%axis equal        % Hacer un grafico con coordenadas de igual tamaño
-title({'Error en el cálculo de la función de alabeo \psi',...
-   'Teórico vs diferencias finitas',},'FontSize', 18) % Escribir el titulo
-% Defino las coordenadas que quiero visualizar [xmin xmax ymin ymax]
-axis([-0.011 0.011 -0.011 0.011]);
-% Dibujo los bordes de la figura
-%plot([-0.01 0.01 0.01 -0.01 -0.01], [-0.01 -0.01 0.01 0.01 -0.01], 'k-', 'LineWidth',5)
+%% Error relativo en el cálculo de la función de alabeo de Saint-Venant
+figure
+plot_esf_3d(xx, yy, diffpsi, ...
+    {'Error relativo en el cálculo de la función de alabeo \psi(x,y)', ...
+     'Teórico vs diferencias finitas'})
 
-%% bye bye
+%% Se dibuja la barra deformada + alabeo
+%% Recupero el borde
+borde = xlsread('torsion_cuadrado.xlsx','borde','Borde');
+borde = flipud(borde);
+borde = [borde(:), [1:numel(borde)]'];
+borde(isnan(borde(:,1)),:) = [];
+borde = sortrows(borde,1);
+ind = borde(:,2);
+ind = [ind; ind(1)];
+
+%% Grafico barra con rotacion
+escala = 1e5;
+LL = linspace(0,400*(x(end)-x(1)),11);
+theta = -theta;
+for z = 1:11
+   zz = LL(z);
+   xnew = cos(theta*zz)*xx(:) - sin(theta*zz)*yy(:);
+   ynew = sin(theta*zz)*xx(:) + cos(theta*zz)*yy(:);
+   SUR(:,:,z) = [xnew(ind),ynew(ind),zz-escala*w(ind)]';
+end
+X = squeeze(SUR(1,:,:));
+Y = squeeze(SUR(2,:,:));
+Z = squeeze(SUR(3,:,:));
+
+figure; 
+surf(Z,Y,X); %axis equal;
+hold on
+for z = 1:11
+   zz = LL(z);
+   xnew = cos(theta*zz)*xx - sin(theta*zz)*yy;
+   ynew = sin(theta*zz)*xx + cos(theta*zz)*yy;
+   surf(zz-escala*w,ynew,xnew);
+end
+
+xlabel('Eje Z')
+ylabel('Eje X')
+zlabel('Eje Y')
+title('Barra deformada')
+
+%camlight
+%lighting gouraud
+colormap([1 1 1]*0.7)
+camproj('perspective')
+material dull
+axis off
+
+return; %bye bye
+
+function plot_esf_2d(x, y, esf, titulo, archivo)
+    global a b borde
+    
+    pcolor(x,y,esf);  % Hacer un dibujo en 2D de los esfuerzos en tau_xz
+    hold on;          % Permitir hacer modificaciones al dibujo anterior
+    shading interp;   % Hacer interpolación de colores
+    hcb = colorbar;   % Poner la barra de colores
+    axis equal tight
+    contour(x,y,esf,20,'k'); % Dibujar 20 curvas de nivel (isobaras)
+    title(titulo);      
+    plot(borde(:,1), borde(:,2), 'k-', 'LineWidth',5)
+
+    % Defino las coordenadas que quiero visualizar [xmin xmax ymin ymax]
+    axis([ -1.1*a 1.1*a -1.1*b 1.1*b ]);
+    
+    xlabel('x [m]');
+    ylabel('y [m]');
+    
+    % Cree un archivo con el gráfico listo para incluir en LaTeX
+    %set(gca,'FontSize',16);   % Números de los ejes x e y
+    %set(hcb,'FontSize',16);   % Fuente en la barra de colores
+    colormap jet
+    
+    if nargin == 5
+        exportgraphics(gcf,archivo,   ...
+            'BackgroundColor','none', ...
+            'ContentType',    'vector')
+    end
+end
+
+function plot_esf_3d(x, y, esf, titulo, archivo)
+    global a b borde
+    
+    mesh(x,y,esf);  % Hacer un dibujo en 2D de los esfuerzos en tau_xz
+    hold on;          % Permitir hacer modificaciones al dibujo anterior
+    shading interp;   % Hacer interpolación de colores
+    hcb = colorbar;   % Poner la barra de colores
+    axis tight
+    contour(x,y,esf,20,'k'); % Dibujar 20 curvas de nivel (isobaras)
+    title(titulo);      
+    plot(borde(:,1), borde(:,2), 'k-', 'LineWidth',5)
+
+    % Defino las coordenadas que quiero visualizar [xmin xmax ymin ymax]
+    % axis([ -1.1*a 1.1*a -1.1*b 1.1*b 0 maxtau]);
+    
+    xlabel('x [m]');
+    ylabel('y [m]');
+    
+    % Cree un archivo con el gráfico listo para incluir en LaTeX
+    %set(gca,'FontSize',16);   % Números de los ejes x e y
+    %set(hcb,'FontSize',16);   % Fuente en la barra de colores
+    colormap jet
+    
+    if nargin == 5
+        exportgraphics(gcf,archivo,   ...
+            'BackgroundColor','none', ...
+            'ContentType',    'vector')
+    end
+end
 
 %{
 mogrify -trim *.png
